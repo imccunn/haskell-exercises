421aba608e247e702daae17ed5e2b07df0bb98d8
Ian D. McCunn <imccunn@expedia.com>
2017-09-21 10:39:15 -0700

    Fix use of list.

--- recursive/List.hs
+++ recursive/List.hs
@@ -1,5 +1,5 @@
 
-data List1 a = Empty1 | Cons1 a (List a) deriving (Show, Read, Eq, Ord)
+data List1 a = Empty1 | Cons1 a (List1 a) deriving (Show, Read, Eq, Ord)
 
 data List a = Empty | Cons { listHead :: a, listTail :: List a} deriving (Show, Read, Eq, Ord)
 
